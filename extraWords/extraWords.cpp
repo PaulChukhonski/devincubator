6966f95e400ea78b653c72925858bb23650462ee
PaulChukhonski <paul.chukhonski@gmail.com>
2021-01-25 12:26:25 +0300

    Added function which removed extra spaces.

--- extraWords/extraWords.cpp
+++ extraWords/extraWords.cpp
@@ -9,17 +9,20 @@ int strLength(char str[]);
 int main()
 {
     char text[SIZE], word[SIZE], prevWord[SIZE] = "";
-    int i, iw = 0, pos;
+    int i, iw = 0;
 
     cout << "Input text: ";
     cin.getline(text, SIZE);
 
-    for (i = 0; text[i] != '\0'; i++) {
-        if (!isSeparator(text[i])) {
+    for (i = 0; text[i] != '\0'; i++) 
+	{
+        if (!isSeparator(text[i])) 
+		{
             word[iw] = text[i];
             iw++;
             
-            if (isSeparator(text[i+1]) || text[i+1] == '\0') {
+            if (isSeparator(text[i+1]) || text[i+1] == '\0') 
+			{
                 word[iw] = '\0';
                 
 				if(!strCompare(prevWord, word))
@@ -36,13 +39,26 @@ int main()
 	            	for(int j = i-iw+1; j <= i; j++)
 					{
 						text[j] = ' '; 
-					}
+					}							
 				}
 				
 				iw = 0;
             }
         }
     }
+ 
+    for (i = 0; text[i] != '\0'; i++) 
+	{
+		for(;text[i] == ' ' && text[i+1] == ' ';)
+		{
+			for(int j = i; j < strLength(text)-1; j++)
+			{
+				text[j] = text[j+1];	
+			}
+		    		    
+		    text[strLength(text)-1] = 0;						
+		}
+    } 
     
     cout << text << endl;
     
